5846bcfc96d6399c59031d63627760a6db43e8e0
emm-jay-ahh <mattrooney.wrk@gmail.com>
2021-06-27 23:08:38 +1000

    utlising some functions now

--- 01_mac_address_changers/maccas_choice.py
+++ 01_mac_address_changers/maccas_choice.py
@@ -2,28 +2,36 @@
 
 import subprocess
 
+def run_ifconfig():
+    subprocess.call(["ifconfig", interface_name])
+    return
+
+def mac_change():
+    # sudo int_name down - hw ether {MAC Change} - int_name up
+    subprocess.call(["sudo", "ifconfig", interface_name, "down"])
+    subprocess.call(["sudo", "ifconfig", interface_name, "hw", "ether", mac_choice])
+    subprocess.call(["sudo", "ifconfig", interface_name, "up"])
+    return
+
+
 interface_name = input("\n\nEnter interface to change MAC of\n\t>>> ")
 mac_choice = input('Enter a new MAC address... format "AA:BB:CC:11:22:33"\n\t>>> ')
  
 print(f"\n---  Changing Mac Address for {interface_name}  ---\n")
 print("  -- OLD --\n")
 
-# run bash commands ifconfig interface_name - view ifconfig before MAC changes
-subprocess.call(["ifconfig", interface_name])
+# run_ifconfig interface_name before MAC is changed
+run_ifconfig()
 
-# int_name down - hw ether {MAC_RANDOM} - int_name up
-subprocess.call(["sudo", "ifconfig", interface_name, "down"])
-subprocess.call(["sudo", "ifconfig", interface_name, "hw", "ether", mac_choice])
-subprocess.call(["sudo", "ifconfig", interface_name, "up"])
-
+# mac_change
+mac_change()
 
 print("\n  -- NEW --\n")
-# run ifconfig interface_name again
-subprocess.call(["ifconfig", interface_name])
+# run ifconfig interface_name again with new MAC
+run_ifconfig()
 
 print("""
 
 IF ANY ERRORS TRY RE-RUNNING maccas_choice.py
 
-###----  MACCAS HAS ENDED  ----###
-""")
+###----  MACCAS HAS ENDED  ----###""")
--- 01_mac_address_changers/maccas_random.py
+++ 01_mac_address_changers/maccas_random.py
@@ -3,28 +3,37 @@
 import subprocess
 import random
 
+def run_ifconfig():
+    subprocess.call(["ifconfig", interface_name])
+    return
+
+def mac_change():
+    # sudo int_name down - hw ether {MAC Change} - int_name up
+    subprocess.call(["sudo", "ifconfig", interface_name, "down"])
+    subprocess.call(["sudo", "ifconfig", interface_name, "hw", "ether", mac_random])
+    subprocess.call(["sudo", "ifconfig", interface_name, "up"])
+    return
+
+
 interface_name = input("\n\nEnter interface to change MAC of\n\t>>> ")
-mac_random = ":".join("%02x"%random.randint(0,255) for x in range(6)) # thanks stackoverflow - get a random MAC
+# thanks stackoverflow random mac gen
+mac_random = ":".join("%02x"%random.randint(0,255) for x in range(6))
 
 print(f"\n---  Changing Mac Address for {interface_name}  ---\n")
 print("  -- OLD --\n")
 
-# run bash commands ifconfig interface_name - view ifconfig before MAC changes
-subprocess.call(["ifconfig", interface_name])
+# run_ifconfig interface_name before MAC is changed
+run_ifconfig()
 
-# int_name down - hw ether {MAC_RANDOM} - int_name up
-subprocess.call(["sudo", "ifconfig", interface_name, "down"])
-subprocess.call(["sudo", "ifconfig", interface_name, "hw", "ether", mac_random])
-subprocess.call(["sudo", "ifconfig", interface_name, "up"])
-
+# mac_change
+mac_change()
 
 print("\n  -- NEW --\n")
-# run ifconfig interface_name again
-subprocess.call(["ifconfig", interface_name])
+# run ifconfig interface_name again with new MAC
+run_ifconfig()
 
 print("""
 
 IF ANY ERRORS TRY RE-RUNNING maccas_random.py
 
-###----  MACCAS HAS ENDED  ----###
-""")
+###----  MACCAS HAS ENDED  ----###""")
